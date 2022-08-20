7924b0af5d1ffcfb67025524b0c74888dba5e209
Oleg <olegkuzmishchev.91@gmail.com>
2022-08-20 11:05:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let number = 7;
+
+// number
+// string - "", '', ``
+// true/false
+// null
+// undefined
+
+// {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+
+// Symbol
+
+console.log(number);
